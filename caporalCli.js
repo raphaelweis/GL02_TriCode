4f596407e030bb1b8bc21e0080b727b073a9dec6
Nicolas Ranvoisy <nicolas.ranvoisy@utt.fr>
2024-12-05 14:42:09 +0100

    Ajout de la fonctionnalité 4

--- caporalCli.js
+++ caporalCli.js
@@ -1,5 +1,7 @@
 const { program } = require('@caporal/core');
 const fs = require('fs');
+const path = require("path");
+const os = require("os");
 const CruParser = require('./CruParser');
 
 program
@@ -66,7 +68,7 @@ program
         }
     })
 
-    //Get-salles (type de cours + salle)
+    //Get-slots
     .command('get-slots', 'Get the time when a room is free')
     .argument('<file>', 'Path to the CRU file')
     .argument('<roomName>', 'Name of the room')
@@ -125,20 +127,123 @@ program
                 }
             }
 
-            const codeEnJour = {L: "Lundi", MA: "Mardi", ME: "Mercredi", J: "Jeudi", V: "Vendredi"};
+            const codeEnJour = {L: "lundi", MA: "mardi", ME: "mercredi", J: "jeudi", V: "vendredi"};
             const listeCodes = ["L","MA","ME","J","V"];
 
             if (coursesInThisRoom.length > 0) {
-                //console.log(JSON.stringify(freeSlots, null, 2));
                 for (let i = 0; i < 5; i++){
                     const creneaux = freeSlots[listeCodes[i]];
-                    for(const creneau of creneaux){
-                        console.log(`Le ${codeEnJour[listeCodes[i]]}, il y a un créneau de ${creneau.start} à ${creneau.end}`);
+                    if(creneaux.length > 0){
+                        for(const creneau of creneaux){
+                            console.log(`Le ${codeEnJour[listeCodes[i]]}, il y a un créneau de ${creneau.start}h à ${creneau.end}h.`);
+                        }
+                    }else{
+                        console.log(`Il n'y a pas de créneau le ${codeEnJour[listeCodes[i]]}.`);
                     }
+                    
                 }
             } else {
-                console.log(`No slot found for room "${roomName}".`);
+                console.log(`No slot found for room "${roomName}". Please verify the room name and try again.`);
             }
+        } catch (err) {
+            console.error(`Error reading file ${filePath}:`, err.message);
+        }
+    })
+
+    //Get-calendar
+    .command('get-calendar', 'Get the calendar associated to a student and his courses between two dates.')
+    .argument('<file>', 'Path to the CRU file')
+    .argument('<startDateEntered>', 'Date that is at the beginning of the calendar')
+    .argument('<endDateEntered>', 'Date that is at the end of the calendar')
+    .argument("[userCoursesName...]", 'Courses of the student.')
+    .action(({ args }) => {
+        const filePath = args.file;
+        const startDateEntered = args.startDateEntered;
+        const endDateEntered = args.endDateEntered;
+        const userCoursesName = args.userCoursesName;
+  
+        try {
+            const fileContent = fs.readFileSync(filePath, 'utf-8');
+            const parser = new CruParser();
+            parser.parse(fileContent);
+  
+            if (!userCoursesName || userCoursesName.length === 0){
+                console.log("Aucun cours fourni. Veuillez entrer au moins un cours.");
+            }
+
+            const userCourses = parser.courses.filter(course => userCoursesName.includes(course.name));
+
+            if(userCourses.length !== userCoursesName.length){
+                console.error("One of the courses you've entered or more does not exist in the database. Please verify the name entered and try again.");
+                return;
+            }
+
+            for(let course of userCourses){
+                course.creneaux = course.creneaux.filter(creneau => creneau.type==="C1" || creneau.type==="D1" || creneau.type==="T1");
+            }
+
+            let icsContent = [];
+
+            icsContent.push("BEGIN:VCALENDAR");
+            icsContent.push("VERSION:2.0");
+            icsContent.push("PRODID:-//MonApplication//MonCalendrier 1.0//FR");
+
+            const startDate = new Date(startDateEntered);
+            const endDate = new Date(endDateEntered);
+            let currentDate = startDate;
+
+            const codeEnNumJour = {L: 1, MA: 2, ME: 3, J: 4, V: 5};
+            const codeEnType = {C1: "CM", D1: "TD", T1: "TP"};
+
+            while (currentDate <= endDate) {
+
+                //console.log(currentDate.toISOString().split("T")[0]);
+                
+                userCourses.forEach((course, index) => {
+                    course.creneaux.forEach((creneau, index2) => {
+
+                        if(codeEnNumJour[creneau.jour] === currentDate.getDay()){
+
+                            icsContent.push("BEGIN:VEVENT");
+                            icsContent.push(`UID:${index}-${Date.now()}`);
+                            icsContent.push(`DTSTAMP:${new Date().toISOString().replace(/[-:]/g, "").split(".")[0]}Z`);
+
+                            const debutDeDate = currentDate.toISOString().replace(/[-:]/g, "").split("T")[0] + "T";
+                            const horaireMatch = creneau.horaire.match(/(\d+):(\d{2})-(\d+):(\d{2})/);
+
+                            let dateDebut, dateFin;
+                            if(horaireMatch[1]<10){
+                                dateDebut = debutDeDate + "0" + horaireMatch[1] + horaireMatch[2];
+                            }else{
+                                dateDebut = debutDeDate + horaireMatch[1] + horaireMatch[2];
+                            }
+                            
+                            if(horaireMatch[3]<10){
+                                dateFin = debutDeDate + "0" + horaireMatch[1] + horaireMatch[2];
+                            }else{
+                                dateFin = debutDeDate + horaireMatch[3] + horaireMatch[4];
+                            }
+                            
+                            icsContent.push(`DTSTART;TZID=Europe/Paris:${dateDebut}00`);
+                            icsContent.push(`DTEND;TZID=Europe/Paris:${dateFin}00`);
+
+                            icsContent.push(`SUMMARY:${codeEnType[creneau.type]} de ${course.name.replace(/^\+/, "")}`);
+                            icsContent.push(`DESCRIPTION:${codeEnType[creneau.type]} de ${course.name.replace(/^\+/, "")} en ${creneau.salle}.`);
+                            icsContent.push(`LOCATION:${creneau.salle}`);
+                            icsContent.push("END:VEVENT");
+                        }
+                        
+                    });
+                });
+
+                currentDate.setDate(currentDate.getDate() + 1);  
+            }
+
+            icsContent.push("END:VCALENDAR");
+
+            const icsFilePath = path.join(path.join(os.homedir(), "Downloads"), "emploi_du_temps.ics");
+            fs.writeFileSync(icsFilePath, icsContent.join("\r\n"), "utf-8");
+
         } catch (err) {
             console.error(`Error reading file ${filePath}:`, err.message);
         }
